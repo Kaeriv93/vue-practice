5c9bc7aff1b1688bedf58f8bef07251821bf34a8
Vireak San <vsan93@yahoo.com>
2022-10-20 11:39:15 -0500

    Used a cdn to have a play around with vue, leaving comments in to better understanding the flow and how things are being rendered

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-Vue.createApp({}).mount('#app')
+//vm stands for viewmodel
+// the world model describes the word and the relationship that vue establishes between 2 parts of on application
+const vm = Vue.createApp({
+    data(){
+        return {
+            firstName: 'John',
+            lastName: 'Doe'
+        }
+    }
+}).mount('#app')
+
+// Vue.createApp({
+//     data(){
+//         return {
+//             firstName: 'Jane',
+//             lastName: 'Doe'
+//         }
+//     }
+// }).mount('#app2')
